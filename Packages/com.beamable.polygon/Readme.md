c30c7b5a048b4462dbf3dee3ab4328cdbd56657c
Chris Hanna <chris@beamable.com>
2023-03-16 13:42:36 -0400

    moving files to polygon package

---
+++ Packages/com.beamable.polygon/Readme.md
@@ -0,0 +1,4 @@
+# Beamable Polygon
+
+Install link.
+https://github.com/beamable/polygon-example.git?path=/Packages/com.beamable.polygon#0.0.0
---
+++ Packages/com.beamable.polygon/Runtime/Common/Api/Inventory/BinaryReference.cs
@@ -0,0 +1,10 @@
+namespace Beamable.Polygon.Common.Api.Inventory
+{
+	public class BinaryReference
+	{
+		public string id;
+		public string uri;
+		public string uploadUri;
+		public string uploadMethod;
+	}
+}
