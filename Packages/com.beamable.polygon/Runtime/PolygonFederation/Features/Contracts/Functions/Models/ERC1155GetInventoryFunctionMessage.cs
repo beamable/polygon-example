60c510fad37d2ca03d9c35b51aadf027fedaf249
Drazen Patekar <dpatekar@gmail.com>
2023-03-27 14:22:15 +0200

    Add a missing output declaration for getInventory function

--- Packages/com.beamable.polygon/Runtime/PolygonFederation/Features/Contracts/Functions/Models/ERC1155GetInventoryFunctionMessage.cs
+++ Packages/com.beamable.polygon/Runtime/PolygonFederation/Features/Contracts/Functions/Models/ERC1155GetInventoryFunctionMessage.cs
@@ -3,7 +3,7 @@ using Nethereum.Contracts;
 
 namespace Beamable.Microservices.PolygonFederation.Features.Contracts.Functions.Models
 {
-    [Function("getInventory", "uint256[], uint256[]")]
+    [Function("getInventory", "uint256[], uint256[], uint256[]")]
     internal class ER1155GetInventoryFunctionMessage : FunctionMessage
     {
         [Parameter("address", "account")]
